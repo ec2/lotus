2c4ab5892bdd6344dd7ea4a97a5cce01cf56a723
Eric Tu <tu.eric@hotmail.com>
2021-01-22 14:42:36 -0500

    v2 genesis

--- build/params_2k.go
+++ build/params_2k.go
@@ -10,31 +10,50 @@ import (
 const BootstrappersFile = ""
 const GenesisFile = ""
 
-const UpgradeBreezeHeight = -1
-const BreezeGasTampingDuration = 0
+// const UpgradeBreezeHeight = -1
+// const BreezeGasTampingDuration = -1
 
-const UpgradeSmokeHeight = -1
-const UpgradeIgnitionHeight = -2
-const UpgradeRefuelHeight = -3
-const UpgradeTapeHeight = -4
+// const UpgradeSmokeHeight = -1
+// const UpgradeIgnitionHeight = -2
+// const UpgradeRefuelHeight = -3
+// const UpgradeTapeHeight = -4
 
-const UpgradeActorsV2Height = 10
-const UpgradeLiftoffHeight = -5
+// const UpgradeActorsV2Height = -1
+// const UpgradeLiftoffHeight = -5
 
-const UpgradeKumquatHeight = 15
-const UpgradeCalicoHeight = 20
-const UpgradePersianHeight = 25
-const UpgradeOrangeHeight = 27
-const UpgradeClausHeight = 30
+// const UpgradeKumquatHeight = 0
+// const UpgradeCalicoHeight = 20
+// const UpgradePersianHeight = 25
+// const UpgradeOrangeHeight = 27
+// const UpgradeClausHeight = 30
+
+const UpgradeBreezeHeight = 100000
+const BreezeGasTampingDuration = 1000001
+
+const UpgradeSmokeHeight = 1000002
+const UpgradeIgnitionHeight = 1000003
+const UpgradeRefuelHeight = 1000004
+const UpgradeTapeHeight = 1000006
+
+const UpgradeActorsV2Height = 1000005
+const UpgradeLiftoffHeight = 1000007
+
+const UpgradeKumquatHeight = 1000008
+const UpgradeCalicoHeight = 1000009
+const UpgradePersianHeight = 1000010
+const UpgradeOrangeHeight = 1000011
+const UpgradeClausHeight = 1000012
 
 var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
 	0: DrandMainnet,
 }
 
 func init() {
+	InsecurePoStValidation = true
 	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)
 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2048))
 	policy.SetMinVerifiedDealSize(abi.NewStoragePower(256))
+	policy.SetPreCommitChallengeDelay(abi.ChainEpoch(15))
 
 	BuildType |= Build2k
 }
@@ -53,3 +72,64 @@ const SlashablePowerDelay = 20
 const InteractivePoRepConfidence = 6
 
 const BootstrapPeerThreshold = 1
+
+// ///// +build debug 2k
+
+// package build
+
+// import (
+// 	"math"
+// 	"os"
+
+// 	"github.com/filecoin-project/go-state-types/abi"
+// 	"github.com/filecoin-project/lotus/chain/actors/policy"
+// )
+
+// const UpgradeBreezeHeight = 10000
+// const BreezeGasTampingDuration = 100000
+
+// const UpgradeSmokeHeight = 100001
+// const UpgradeIgnitionHeight = 100002
+// const UpgradeRefuelHeight = 100003
+// const UpgradeTapeHeight = 100004
+
+// var UpgradeActorsV2Height = abi.ChainEpoch(100000)
+// var UpgradeLiftoffHeight = abi.ChainEpoch(100045)
+
+// const UpgradeKumquatHeight = 15
+// const UpgradeCalicoHeight = 20
+// const UpgradePersianHeight = 25
+
+// var DrandSchedule = map[abi.ChainEpoch]DrandEnum{
+// 	0: DrandMainnet,
+// }
+
+// func init() {
+// 	InsecurePoStValidation = true
+
+// 	policy.SetSupportedProofTypes(abi.RegisteredSealProof_StackedDrg2KiBV1)
+// 	policy.SetConsensusMinerMinPower(abi.NewStoragePower(2048))
+// 	policy.SetMinVerifiedDealSize(abi.NewStoragePower(256))
+// 	policy.SetPreCommitChallengeDelay(abi.ChainEpoch(15))
+// 	if os.Getenv("LOTUS_DISABLE_V2_ACTOR_MIGRATION") == "1" {
+// 		UpgradeActorsV2Height = math.MaxInt64
+// 		UpgradeLiftoffHeight = 100004
+// 	}
+
+// 	BuildType |= Build2k
+// }
+
+// const BlockDelaySecs = uint64(4)
+
+// const PropagationDelaySecs = uint64(1)
+
+// // SlashablePowerDelay is the number of epochs after ElectionPeriodStart, after
+// // which the miner is slashed
+// //
+// // Epochs
+// const SlashablePowerDelay = 20
+
+// // Epochs
+// const InteractivePoRepConfidence = 6
+
+// const BootstrapPeerThreshold = 1
--- node/modules/testing/genesis.go
+++ node/modules/testing/genesis.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/ipfs/go-blockservice"
 	"github.com/ipfs/go-cid"
+	"github.com/ipfs/go-datastore"
 	offline "github.com/ipfs/go-ipfs-exchange-offline"
 	logging "github.com/ipfs/go-log/v2"
 	"github.com/ipfs/go-merkledag"
@@ -20,6 +21,8 @@ import (
 	"github.com/filecoin-project/lotus/build"
 	"github.com/filecoin-project/lotus/chain/gen"
 	genesis2 "github.com/filecoin-project/lotus/chain/gen/genesis"
+	"github.com/filecoin-project/lotus/chain/stmgr"
+	"github.com/filecoin-project/lotus/chain/store"
 	"github.com/filecoin-project/lotus/chain/types"
 	"github.com/filecoin-project/lotus/chain/vm"
 	"github.com/filecoin-project/lotus/genesis"
@@ -78,8 +81,26 @@ func MakeGenesis(outFile, genesisTemplate string) func(bs dtypes.ChainBlockstore
 			if err != nil {
 				return nil, xerrors.Errorf("make genesis block: %w", err)
 			}
-
 			fmt.Printf("GENESIS MINER ADDRESS: t0%d\n", genesis2.MinerStart)
+			cs := store.NewChainStore(bs, bs, datastore.NewMapDatastore(), syscalls, journal.NilJournal())
+			sm := stmgr.NewStateManager(cs)
+			ts, err := types.NewTipSet([]*types.BlockHeader{b.Genesis})
+			if err != nil {
+				return nil, xerrors.Errorf("Failed to greateafeawefawef ")
+			}
+			s, err := stmgr.UpgradeActorsV2(context.TODO(), sm, nil, b.Genesis.ParentStateRoot, b.Genesis.Height, ts)
+			if err != nil {
+				return nil, xerrors.Errorf("failed to upgrade: %w\n", err)
+			}
+			b.Genesis.ParentStateRoot = s
+
+			mmb, err := b.Genesis.ToStorageBlock()
+			if err != nil {
+				return nil, xerrors.Errorf("serializing msgmeta failed: %w", err)
+			}
+			if err := bs.Put(mmb); err != nil {
+				return nil, xerrors.Errorf("putting msgmeta block to blockstore: %w", err)
+			}
 
 			f, err := os.OpenFile(outFile, os.O_CREATE|os.O_WRONLY|os.O_TRUNC, 0644)
 			if err != nil {
@@ -87,7 +108,9 @@ func MakeGenesis(outFile, genesisTemplate string) func(bs dtypes.ChainBlockstore
 			}
 
 			offl := offline.Exchange(bs)
+
 			blkserv := blockservice.New(bs, offl)
+
 			dserv := merkledag.NewDAGService(blkserv)
 
 			if err := car.WriteCarWithWalker(context.TODO(), dserv, []cid.Cid{b.Genesis.Cid()}, f, gen.CarWalkFunc); err != nil {
